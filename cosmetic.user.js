49580e3e1a2312d30cd4e4e169c3b56d5bb68233
luxysiv <luxysiv@users.noreply.github.com>
2023-01-29 01:11:47 +0000

    Cập nhật userscript: 2023-01-29

--- cosmetic.user.js
+++ cosmetic.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Cosmetic Ad Block for Bromite
 // @namespace    luxysiv
-// @version      2023.01.28
+// @version      2023.01.29
 // @description  Blocks annoying elements in pages, sourced from many different filter lists
 // @author       luxysiv 
 // @match        *://*/*
@@ -14,7 +14,7 @@
 /// @stats blockers for 23567 domains, injected CSS rules for 3996 domains
 {
     let log = function (...data) {
-        console.log("[Cosmetic filters by luxysiv (v2023.01.28 full)]:", ...data);
+        console.log("[Cosmetic filters by luxysiv (v2023.01.29 full)]:", ...data);
     }
 
 
