477a99b1480917d3b8b51d5e579002d6539148cd
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-06-30 14:55:47 +0700

    Update generate-all.sh

--- generate/generate-all.sh
+++ generate/generate-all.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash 
 set -euo pipefail 
 
+
 echo "::group::Generating cosmetic filter script"
 cd cosmetic
 bash ./generate.sh
 cd ..
 echo "::endgroup::"
 
-# Using GitHub Actions environment variables directly
-git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-user_name=$(gh api /users/${GITHUB_REPOSITORY_OWNER} | jq .name -r)
-git config --global user.name "${user_name}"
-
-git add cosmetic.user.js || { echo "Failed to add the userscript to repo"; exit 1; }
-git commit -m "Update userscript" --author="${user_name} <${{ secrets.GITHUB_EMAIL }}>" || { echo "Failed to commit the userscript to repo"; exit 1; }
-git push origin main || { echo "Failed to push the userscript to repo"; exit 1; }
+git config --global user.email "${GITHUB_ACTOR_ID}+${GITHUB_ACTOR}@users.noreply.github.com"
+git config --global user.name "$(gh api /users/${GITHUB_ACTOR} | jq .name -r)"
+git add cosmetic.user.js || error "Failed to add the userscript to repo"
+git commit -m "Update userscript" --author=. || error "Failed to commit the userscript to repo"
+git push origin main || error "Failed to push the userscript to repo"
