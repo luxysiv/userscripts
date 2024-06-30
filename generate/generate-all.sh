df110e39d954d139ef7dcc0fbe6ed283871e696a
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-06-30 14:15:38 +0700

    Update generate-all.sh

--- generate/generate-all.sh
+++ generate/generate-all.sh
@@ -7,3 +7,9 @@ cd cosmetic
 bash ./generate.sh
 cd ..
 echo "::endgroup::"
+
+git config --global user.email "${GITHUB_ACTOR_ID}+${GITHUB_ACTOR}@users.noreply.github.com"
+git config --global user.name "$(gh api /users/${GITHUB_ACTOR} | jq .name -r)"
+git add cosmetic.user.js || error "Failed to add the userscript to repo"
+git commit -m "Update userscript" --author=. || error "Failed to commit the userscript to repo"
+git push origin main || error "Failed to push the userscript to repo"
