e3a3a408a8327f4cf0f29ecaf46e1824ff923356
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2026-07-12 02:55:00 +0000

    Update userscript

--- cosmetic.user.js
+++ cosmetic.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Cosmetic Ad Block for Browser
 // @namespace    luxysiv
-// @version      2026.07.11
+// @version      2026.07.12
 // @description  Blocks annoying elements in pages, sourced from many different filter lists
 // @author       luxysiv 
 // @match        *://*/*
@@ -15,7 +15,7 @@
 /// @stats blockers for 7570 domains, injected CSS rules for 0 domains
 {
     let log = function (...data) {
-        console.log("[Cosmetic filters by luxysiv (v2026.07.11 full)]:", ...data);
+        console.log("[Cosmetic filters by luxysiv (v2026.07.12 full)]:", ...data);
     }
 
 
